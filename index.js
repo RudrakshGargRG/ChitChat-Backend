f635e7a1b6587852078476c0f190ad49d3901c9c
Rudraksh Garg <rudrakshgarg@Rudrakshs-MacBook-Pro.local>
2023-06-29 11:37:47 +0530

    cors origin fix

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const server = app.listen(process.env.PORT || 8080 , () => {
 
 const io = socket(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: ["https://chitchatanytime.netlify.app", "http://localhost:3000"],
       credentials: true,
     },
   });
